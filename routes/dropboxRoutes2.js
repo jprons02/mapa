357d52a486c2442fc5ce0ba5f2f35313063db9f8
Joseph Ronselli <joseph.ronselli@gmail.com>
2020-05-16 10:08:37 -0400

    dropbox route2 with multer working with files up to 150MB

--- routes/dropboxRoutes2.js
+++ routes/dropboxRoutes2.js
@@ -12,61 +12,66 @@ const upload = multer({dest: 'temp_files_to_upload'});
 
 
 //100% have to use multer... the functions that were listening for data were firing before axios calls could resolve.
-//-accept data in formData type format from front end 
+//-accept data in formData type format from frontend 
 //-then store in file path in backend
 //-then send to dropbox from filepath
 
 
-/*
-fs.access(path[, mode], callback)
-fs.access(file, fs.constants.F_OK, (err) => {
-  console.log(`${file} ${err ? 'does not exist' : 'exists'}`);
-});
-
-*/
-
 module.exports = app => {
 
-    app.post('/api/testupload/:file/:size', upload.single('myFile'), async (req, res) => {
+    app.post('/api/testupload/:file/:size', upload.single('myFile'), (req, res) => {
 
         try {
-            if(req.file.filename) {
-                console.log("file is downloaded? do axios stuff");
-                console.log(req.file.filename);
-                //console.log(req.body);
-                //res.send(req.file);
-                //can do crazy axios stuff now.
+            const filePath = `temp_files_to_upload/${req.file.filename}`;
 
-                //test the fs.access stuff here...
+            //checks if file is uploaded to temp_files_to_upload folder
+            fs.access(filePath, fs.constants.F_OK, async (err) => {
+                console.log(`${filePath} ${err ? 'does not exist' : 'exists'}`);
+                if(err) {
+                    console.log('file has not been uploaded');
+                }
+                //file exists, do axios call to dropbox
+                else {
+                    console.log('file has been uploaded.');
+                    console.log(req.file);
+                    
+                    const url = 'https://content.dropboxapi.com/2/files/upload';
+                    const response = await axios({
+                        method: 'POST',
+                        url: url,
+                        headers: {
+                            'Content-Type': 'application/octet-stream',
+                            'Authorization': `Bearer ${dropboxAccessToken}`,
+                            'Dropbox-API-Arg': JSON.stringify({
+                                'path': `/media/${req.file.originalname}`,
+                                'mode': 'add',
+                                'autorename': true,
+                                'mute': false,
+                                'strict_conflict': false
+                            })
+                        },
+                        //max bodylength limit error solved: https://stackoverflow.com/questions/56868023/error-request-body-larger-than-maxbodylength-limit-when-sending-base64-post-req/56868296
+                        'maxContentLength': Infinity,
+                        'maxBodyLength': Infinity,
+                        data: fs.createReadStream(`temp_files_to_upload/${req.file.filename}`)
+                    })
+                    res.send(response.data);
 
-                /*
-                const response = await axios({
-                    method: 'POST',
-                    url: url,
-                    headers: {
-                        'Content-Type': 'application/octet-stream',
-                        'Authorization': `Bearer ${dropboxAccessToken}`,
-                        //JSON.stringify needed because dropbox api requires "Dropbox-API-Arg" value to be JSON.
-                        //ref for json.stringify: https://www.dropboxforum.com/t5/Dropbox-API-Support-Feedback/quot-Dropbox-API-Arg-quot-could-not-decode-input-as-JSON/td-p/288054
-                        'Dropbox-API-Arg': JSON.stringify({
-                            //'path': '/media/test_7.txt',
-                            'path': `/media/${req.params.file}`,
-                            'mode': 'add',
-                            'autorename': true,
-                            'mute': false,
-                            'strict_conflict': false
-                        })
-                    },
-                    //data:  '@/files/test_2.txt'//this is working for local file. filepath from where you are uploading. 
-                    //data:  `@/temp_files_to_upload/${req.params.file}`
-                    //data: `@/temp_files_to_upload/${req.file.filename}`
-                    data: '@/temp_files_to_upload/73a068a3b8cba5ba6ade8a45444f153a'
-                })
-                res.send(response.data);
-                */
-                
-
-            }
+                    //deletes file from temp_files_to_upload upon successful upload to dropbox.
+                    //https://stackoverflow.com/questions/5315138/node-js-remove-file
+                    if(response.data) {
+                        console.log('destroy temp file that is now in dropbox.');
+                        (async () => {
+                            try {
+                              await fs.promises.unlink(`temp_files_to_upload/${req.file.filename}`);
+                            } catch (e) {
+                              console.log(e);
+                            }
+                        })();
+                    }
+                }
+            });
+            
         }
         catch(error) {
           res.send(error);
