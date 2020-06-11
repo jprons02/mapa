68c8c2f05fe476b89d022a955cac96de5cf72449
Joseph Ronselli <joseph.ronselli@gmail.com>
2020-06-11 09:09:44 -0400

    working on production keys

--- config/prod.js
+++ config/prod.js
@@ -1,1 +1,17 @@
 //prod.js = production keys here
+
+module.exports = {
+    adminID: process.env.adminID,
+    adminPassword: process.env.adminPassword,
+    designerId: process.env.designerId,
+    designerPassword: process.env.designerPassword,
+    mediaHouseId: process.env.mediaHouseId,
+    mediaHousePassword: process.env.mediaHousePassword,
+    mongoURI: process.env.mongoURI,
+    googleClientID: process.env.googleClientID,
+    googleClientSecret: process.env.googleClientSecret,
+    cookieKey: process.env.cookieKey,
+    dropboxKey: process.env.dropboxKey,
+    dropboxSecret: process.env.dropboxSecret,
+    dropboxAccessToken: process.env.dropboxAccessToken
+}
