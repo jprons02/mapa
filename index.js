d8b7dad02f10da921d720b24a55a3818c25cfcd9
Joseph Ronselli <joseph.ronselli@gmail.com>
2020-06-11 16:02:30 -0400

    postbuild package.json fix

--- index.js
+++ index.js
@@ -53,12 +53,20 @@ loginRoute(app);
 googleOAuthRoute(app);
 dropboxRoutes(app, io);
 
-
 app.use('/admin/tools/testing', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+if(process.env.NODE_ENV === 'production') {
+    //express will serve up production assets like our main.js file or main.css file
+    app.use(express.static('client/build'));
 
+    //express will serve up the index.html file if it doesnt recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     console.log('Server listening on port ' + PORT)
