e288f5d5e6966ddc3982ad8fc74b9d532414c211
Joseph Ronselli <joseph.ronselli@yahoo.com>
2020-06-19 10:54:33 -0400

    jackpot number validation

--- client/src/components/ChangeJackpotNumber.js
+++ client/src/components/ChangeJackpotNumber.js
@@ -8,55 +8,85 @@ class ChangeJackPotNumber extends React.Component {
         super(props);
         this.state = {
             value: '',
-            isWorking: false,
-            isSubmit: false
+            isSubmit: false,
+            showInvalidError: false
         };
     }
     
 
     changeNumber = async () => {
-        //is the number a string?
-        const url = '/api/jackpotnumber/';
-        const data = {
-            number: this.state.value
+        //validate number
+        if(!this.validateNumber()) {
+            this.setState({showInvalidError: true})
         }
-        
-        try {
-            const response = await axios({
-                method: 'POST',
-                url: url,
-                data: data
-            })
-            if(response.data) {
-                console.log(response.data);
-                this.setState({
-                    isSubmit: true
+        else {
+            const url = '/api/jackpotnumber/';
+            const data = {
+                number: this.state.value
+            }
+            
+            try {
+                const response = await axios({
+                    method: 'POST',
+                    url: url,
+                    data: data
                 })
+                if(response.data) {
+                    console.log(response.data);
+                    this.setState({
+                        isSubmit: true
+                    })
+                }
+            }
+            catch(error){
+                console.log(error);
             }
         }
-        catch(error){
-            console.log(error);
+    }
+
+    validateNumber = () => {
+        if(this.state.value) {
+            const value = parseInt(this.state.value);
+            //reason: doing nan test, 100abcd is still passing. so 100 == '100abcd' will result in false in javascript.
+            if(value == this.state.value) {
+                //not valid
+                if (isNaN(value)) {
+                    return false
+                //valid
+                } else {
+                    return true
+                }
+            }
         }
+        return false  
     }
 
     handleChange = (event) => {
         this.setState({value: event.target.value});
-        this.setState({isSubmit: false})
+        this.setState({isSubmit: false});
+        this.setState({showInvalidError: false});
     }
-    
+
 
     render() {
+        const successOrError = this.state.isSubmit ? 'success' : this.state.showInvalidError ? 'error' : '';
+
         return (
             <React.Fragment>
                 <Header as='h2'>Jackpot Number</Header>
-                <Form className={this.state.isSubmit ? 'success' : ''}>
-                    <Form.Input width={4} onChange={this.handleChange} value={this.state.value} />
+                <Form className={successOrError}>
+                    <Form.Input width={4} onChange={this.handleChange} value={this.state.value} placeholder='Enter number here'/>
                     <Message
                         success
                         header='Jackpot Number updated'
                         content={`${this.state.value} has been set.`}
                     />
-                    <Button style={{marginTop: '14px'}} loading={this.state.isWorking} onClick={this.changeNumber}>Submit</Button>
+                    <Message
+                        error
+                        header='Please type in a valid number.'
+                        content='Do not use " , " or " $ " in the number.'
+                    />
+                    <Button onClick={this.changeNumber}>Submit</Button>
                 </Form>
             </React.Fragment>
         )
