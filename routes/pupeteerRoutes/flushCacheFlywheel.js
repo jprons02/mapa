cbae5ada9382f23cdd00c0b73f866bbeadfa214c
Joseph Ronselli <joseph.ronselli@gmail.com>
2020-10-28 15:33:54 -0400

    more accurate dom element selecting for websites in pupeteer

--- routes/pupeteerRoutes/flushCacheFlywheel.js
+++ routes/pupeteerRoutes/flushCacheFlywheel.js
@@ -1,18 +1,19 @@
 const axios = require("axios");
+const {
+  websecurityscanner,
+} = require("googleapis/build/src/apis/websecurityscanner");
 const puppeteer = require("puppeteer");
 const { flywheelID, flywheelPW } = require("../../config/keys");
 
 module.exports = (app) => {
   app.get("/api/p/flushcache", async (req, res) => {
-    var isMiccosukeeCacheFlushed = false;
-    var isMrgCacheFlushed = false;
+    const websites = [
+      { title: "Miccosukee.com", isCacheFlushed: false },
+      { title: "Resort & Gaming", isCacheFlushed: false },
+    ];
 
-    //improve this selector?... maybe innerhtml == 'miccosukee' or something like that...
-    const miccosukeeSelector =
-      "#advanced-site-list-with-saved-filters > div > div.filter-contain > div > div.site-container > div:nth-child(5) > div.site__inner > div.site__content > h2 > a";
-
-    const mrgSelector =
-      "#advanced-site-list-with-saved-filters > div > div.filter-contain > div > div.site-container > div:nth-child(7) > div.site__inner > div.site__content > h2 > a";
+    const flywheelWebsiteSectionSelector =
+      "#advanced-site-list-with-saved-filters > div > div.filter-contain > div > div.site-container";
 
     const loginButtonSelector =
       "#main > section > div > div.card.--mt-30 > form > div.card__footer > button";
@@ -20,19 +21,18 @@ module.exports = (app) => {
     const advancedMenuSelector =
       "#main > section.--full > div > div.layout__nav > div.layout__nav-left.shadow-right > div > ul > li:nth-child(6) > a";
 
-    const websites = [miccosukeeSelector, mrgSelector];
-
     //type in username/password and click login
     const loginToHosting = async (websites, fcCallback, responseCallback) => {
       for (const website of websites) {
-        //headless: true will open a browser and show the automation
+        //headless: false will open a browser and show the automation
         try {
-          const browser = await puppeteer.launch({ headless: true });
+          const browser = await puppeteer.launch({ headless: false });
           const page = await browser.newPage();
           await page.goto("https://app.getflywheel.com/login");
           await page.type("#username", flywheelID);
           await page.type("#password", flywheelPW);
           await page.click(loginButtonSelector);
+
           if (websites.length - 1 === websites.indexOf(website)) {
             //this is the last item in the array, call the sendResponse funtion.
             await fcCallback(browser, page, website, responseCallback);
@@ -54,9 +54,16 @@ module.exports = (app) => {
       responseCallback
     ) => {
       try {
-        //wait for website selections to load and select miccosukee
-        await page.waitForSelector(website);
-        await page.click(website);
+        //wait for section to load then create website selector variables
+        await page.waitForSelector(flywheelWebsiteSectionSelector);
+        await page.evaluate((website) => {
+          //Array.from is used to translate the Nodelist returned from querySelector to an array in order to use .find method
+          const websiteElements = Array.from(document.querySelectorAll("a"));
+          const websiteSelector = websiteElements.find(
+            (element) => element.innerText === website.title
+          );
+          websiteSelector.click();
+        }, website);
 
         //make sure "advanced" menu is loaded and click advanced
         await page.waitForSelector(advancedMenuSelector);
@@ -84,17 +91,7 @@ module.exports = (app) => {
             { timeout: 0 }
           )
           .then(() => {
-            if (website === miccosukeeSelector) {
-              isMiccosukeeCacheFlushed = true;
-              console.log(
-                "isMiccosukeeCacheFlushed is set to: ",
-                isMiccosukeeCacheFlushed
-              );
-            }
-            if (website === mrgSelector) {
-              isMrgCacheFlushed = true;
-              console.log("isMrgCacheFlushed is set to: ", isMrgCacheFlushed);
-            }
+            website.isCacheFlushed = true;
           });
         await browser.close();
         //if callback argument exists, this is the last iteration - call sendResponse function
@@ -109,7 +106,7 @@ module.exports = (app) => {
     };
 
     const sendResponse = () => {
-      if (isMiccosukeeCacheFlushed && isMrgCacheFlushed) {
+      if (websites[0].isCacheFlushed && websites[1].isCacheFlushed) {
         res.send("CACHE FLUSHED!");
       } else {
         res.send("ERROR: CACHE NOT FLUSHED");
